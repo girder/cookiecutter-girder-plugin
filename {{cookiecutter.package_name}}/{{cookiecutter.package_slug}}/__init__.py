a5c864dbafb14d731fcc8a4a22d20fc678fb0e43
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-10-26 10:15:16 -0400

    Remove an unnecessary comma from the plugin class

--- {{cookiecutter.package_name}}/{{cookiecutter.package_slug}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_slug}}/__init__.py
@@ -2,7 +2,7 @@ from girder import plugin
 
 
 class GirderPlugin(plugin.GirderPlugin):
-    DISPLAY_NAME = '{{ cookiecutter.plugin_name.replace("\'", "\\\'") }}',
+    DISPLAY_NAME = '{{ cookiecutter.plugin_name.replace("\'", "\\\'") }}'
     {%- if cookiecutter.include_web_client_plugin == 'yes' %}
     CLIENT_SOURCE_PATH = 'web_client'{% endif %}
 
