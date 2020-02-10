658ce13a6a686aac5ebf7bad3dfebea470351f6b
Dan LaManna <danlamanna@users.noreply.github.com>
2020-02-09 22:39:12 -0500

    Add python 3.8 classifier

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -19,7 +19,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     description='{{ cookiecutter.short_description }}',
     install_requires=requirements,
