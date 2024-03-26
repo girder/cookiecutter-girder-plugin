b906315c13cc907870f87cae7a6a8932c4d5f9a1
Zach Mullen <zach.mullen@kitware.com>
2024-03-26 16:24:01 -0400

    Update cookiecutter for Girder v5

--- {{cookiecutter.package_name}}/{{cookiecutter.package_slug}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_slug}}/__init__.py
@@ -1,11 +1,22 @@
+{%- if cookiecutter.include_web_client_plugin == 'yes' %}
+from pathlib import Path
+{% endif %}
 from girder import plugin
 
 
 class GirderPlugin(plugin.GirderPlugin):
     DISPLAY_NAME = '{{ cookiecutter.plugin_name.replace("\'", "\\\'") }}'
-    {%- if cookiecutter.include_web_client_plugin == 'yes' %}
-    CLIENT_SOURCE_PATH = 'web_client'{% endif %}
 
     def load(self, info):
         # add plugin loading logic here
+        {%- if cookiecutter.include_web_client_plugin == 'yes' %}
+        plugin.registerPluginStaticContent(
+            plugin='{{ cookiecutter.package_slug }}',
+            css=['/style.css'],
+            js=['/girder-plugin-{{ cookiecutter.package_name }}.umd.cjs'],
+            staticDir=Path(__file__).parent / 'web_client' / 'dist',
+            tree=info['serverRoot'],
+        )
+        {%- else %}
         pass
+        {%- endif %}
